71809eb19b83247b0e2c5ff6753fc52ea71a88c4
sigmajove <sigmajove@gmail.com>
2024-06-10 23:59:31 -0700

    The first version didn't actually work.

--- public/client.js
+++ public/client.js
@@ -1,10 +1,10 @@
 const getSocketId = new Promise((resolve) => {
     fetch(`${myURL}/getid`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }).then(response => response.json())
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(response => response.json())
         .then((response) => {
             const socketId = response.id;
             const socket = io({
@@ -31,37 +31,36 @@ const getSocketId = new Promise((resolve) => {
                 body: JSON.stringify({
                     socketId: socketId
                 })
-            }).then(() => resolve(socketId));
+            }).then(() => {
+                const oldName = sessionStorage.getItem("player");
+                if (oldName === null) {
+                    fetch(`${myURL}/disconnected`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            }
+                        }).then(response => response.json())
+                        .then((response) => {
+                            const disconnected = response.disconnected;
+                            if (disconnected.length == 0) {
+                                nameInput();
+                            } else {
+                                // List all the disconnected players as part of
+                                // the name input menu.
+                                chooseName(disconnected);
+                            }
+                        });
+                } else {
+                    // See if we can get the old name.
+                    setName(oldName, socketId);
+                }
+                resolve(socketId);
+            });
         });
 });
 
-{
-    const oldName = sessionStorage.getItem("player");
-    if (oldName === null) {
-        fetch(`${myURL}/disconnected`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }).then(response => response.json())
-            .then((response) => {
-                const disconnected = response.disconnected;
-                if (disconnected.length == 0) {
-                    nameInput();
-                } else {
-                    // List all the disconnected players as part of
-                    // the name request.
-                    chooseName(disconnected);
-                }
-            });
-    } else {
-        // See if we can get the old name.
-        getSocketId.then((socketId) => setName(oldName, socketId));
-    }
-}
-
-function setName(player, socketId) {
-    fetch(`${myURL}/setname`, {
+async function setName(player, socketId) {
+    var response = await fetch(`${myURL}/setname`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -70,55 +69,37 @@ function setName(player, socketId) {
             player: player,
             uuid: socketId
         })
-    }).then(response => response.json())
-        .then((response) => {
-            const error = response.error;
-            if (error !== undefined) {
-                console.error(error);
-            } else {
-                let player = response.player;
-                if (player !== undefined) {
-                    document.getElementById("choosename").style.display =
-                        "none";
-                    setPageName(player, socketId);
-                    sessionStorage.setItem("player", player);
-                }
-                let inuse = response.inuse;
-                if (inuse !== undefined) {
-                    document.getElementById("choosename").innerHTML =
-                        `<div>The name ${player} is in use.</div>
+    });
+    response = await response.json();
+    console.log("setname returns", response);
+    {
+        const error = response.error;
+        if (error !== undefined) {
+            console.error(error);
+            return;
+        }
+    } {
+        const player = response.player;
+        if (player !== undefined) {
+            document.getElementById("choosename").style.display =
+                "none";
+            setPageName(player, socketId);
+            sessionStorage.setItem("player", player);
+            return;
+        }
+    } {
+        const inuse = response.inuse;
+        if (inuse !== undefined) {
+            document.getElementById("choosename").innerHTML =
+                `<div>The name ${player} is in use.</div>
 Select a new name <input type="text" id="newplayer"><br>
 <button onclick="namePlayer(true)">Submit</button>`;
-                } else {
-                    console.error("Unexpected response from /setname");
-                    console.error(response);
-                }
-            }
-        });
-}
-
-fetch(`${myURL}/disconnected`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    }
-}).then(response => response.json())
-    .then((response) => {
-        const oldName = sessionStorage.getItem("player");
-        const connected = response.connected;
-        const disconnected = response.disconnected;
-        if (oldName === null) {
-            if (disconnected.length == 0) {
-                nameInput();
-            } else {
-                // List all the disconnected players as part of
-                // the name request.
-                chooseName(disconnected);
-            }
-        } else {
-            getSocketId.then((socketId) => setName(oldName, socketId));
+            return;
         }
-    });
+    }
+    console.error("Unexpected response from /setname");
+    console.error(response);
+}
 
 function foundPlayer(player) {
     sessionStorage.setItem("player", player);
@@ -156,7 +137,6 @@ function nameInput() {
 }
 
 async function namePlayer(retry) {
-    const socketId = await getSocketId;
     let p;
     if (retry) {
         p = document.getElementById("newplayer").value.trim();
@@ -167,29 +147,7 @@ async function namePlayer(retry) {
         }
     }
     if (p != "") {
-        setName(p, socketId).then(response => response.json())
-            .then((response) => {
-                let error = response.error;
-                if (error !== undefined) {
-                    console.error(error);
-                    return;
-                }
-                let player = response.player;
-                if (player !== undefined) {
-                    document.getElementById("choosename").style.display =
-                        "none";
-                    setPageName(player, socketId);
-                }
-                let inuse = response.inuse;
-                if (inuse !== undefined) {
-                    document.getElementById("choosename").innerHTML =
-                        `<div>The name ${p} is in use.</div>
-Select a new name <input type="text" id="newplayer"><br>
-<button onclick="namePlayer(true)">Submit</button>`;
-                    return;
-                }
-
-            });
+        setName(p, await getSocketId);
     };
 }
 
